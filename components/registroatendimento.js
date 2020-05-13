00976d3527681d4cfd212c45b7bdcbfe007d87a7
Gustavo Calixto <gmcalixto@gmail.com>
2020-05-13 12:04:34 -0300

    Implementação de Modal

--- components/registroatendimento.js
+++ components/registroatendimento.js
@@ -1,10 +1,20 @@
 import React, {Component,useState} from 'react';
-import {Text,View,StyleSheet} from 'react-native';
+import {Text,View,StyleSheet,Modal,TouchableHighlight} from 'react-native';
 import PropTypes from 'prop-types';
 
 
 
 export default class RegistroAtendimento extends React.Component {
+
+  //state para controlar visibilidade do modal
+  state = {
+    modalVisible: false,
+  }
+
+  //troca o modo de visibilidade do modal
+  alteraModal(visibilidade){
+    this.setState({modalVisible: visibilidade});
+  }
 
 
   //construtor para uso do props
@@ -29,14 +39,44 @@ export default class RegistroAtendimento extends React.Component {
   //renderização do componente
   render(){
     return(
-      <View style={styles.container}>
-        <Text style={styles.outros}>Data de Atendimento: {this.props.dataAtendimento}</Text>
-        <Text style={styles.outros}>Sintomas: {this.props.sintomas}</Text>
-        <Text style={styles.outros}>Doenças Pré-existentes: {this.props.doencaPreexistente}</Text>
-        <Text style={styles.outros}> 
-          Status COVID: <Text style={this.covidColor}>{this.props.statusCovid}</Text>
-        </Text>
-      </View>
+      <View>
+        
+        <Modal
+          animationType = {'slide'}
+          transparent = {false}
+          visible = {this.state.modalVisible}
+          onRequestClose= {() => console.log('Modal fechado')}
+        >
+
+          <View style={styles.modal}>
+            <Text style={styles.outros}>Data de Atendimento: {this.props.dataAtendimento}</Text>
+            <Text style={styles.outros}>Sintomas: {this.props.sintomas}</Text>
+            <Text style={styles.outros}>Doenças Pré-existentes: {this.props.doencaPreexistente}</Text>
+            <Text style={styles.outros}> 
+              Status COVID: <Text style={this.covidColor}>{this.props.statusCovid}</Text>
+            </Text>
+
+            <TouchableHighlight
+              onPress={
+                ()=>{
+                  this.alteraModal(!this.state.modalVisible)
+                }}>
+                    <Text style={styles.button}>Fechar</Text>
+            </TouchableHighlight>
+
+          </View>
+
+        </Modal>
+
+
+        <TouchableHighlight
+          onPress={
+            ()=>{
+              this.alteraModal(!this.state.modalVisible)
+            }}>
+          <Text style={styles.button}>Detalhes</Text>
+        </TouchableHighlight>
+     </View>
     );
 
 
@@ -44,22 +84,26 @@ export default class RegistroAtendimento extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-      borderColor: 'black',
-      borderWidth: 1
-  },
-  covid: { 
-          width:'100%',
-          margin: 5,
-          textAlign: 'left',
-          fontSize: 15
-        },
   outros: {
           width:'100%',
           margin: 5,
           textAlign: 'left',
-          fontSize: 10,
+          fontSize: 20,
           fontStyle:'italic',
+  },
+  button:{
+    borderColor:'black',
+    borderWidth: 1,
+    fontSize: 20,
+    backgroundColor: 'pink',
+    textAlign: 'center'
+  },
+  modal: {
+    borderColor: 'blue',
+    borderWidth: 1,
+    fontSize: 20,
+    marginTop: 200,
+    padding: 20
   }
 });
 
