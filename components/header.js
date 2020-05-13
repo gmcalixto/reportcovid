8a903c41a31ca4b6bfd753f502402fa48a956a87
Gustavo Calixto <gmcalixto@gmail.com>
2020-05-12 23:22:58 -0300

    Adicionando data e hora atual para o header do relatório

--- components/header.js
+++ components/header.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {View,Text,StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
+
+//gerando data e hora atual
+var now = new Date().toLocaleString();
 
 export default class Header extends React.Component {
 
+  
   //construtor para uso do props
   constructor(props){
     super(props);
@@ -12,16 +16,25 @@ export default class Header extends React.Component {
   //renderização do componente
   render(){
     return(
-      <Text style={{width:'100%',
-                    borderWidth: 2,
-                    padding: 10,
-                    fontSize: 20,
-                    textAlign:'left',
-                    backgroundColor:'red'}}>{this.props.headerText}</Text>
+      <View style={styles.container}>
+        <Text>{this.props.headerText}</Text>
+        <Text style={{fontSize: 10}}>Gerado em {now}</Text>
+      </View>
     );
 
 
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+                      width:'100%',
+                      borderWidth: 2,
+                      padding: 10,
+                      fontSize: 20,
+                      textAlign:'left',
+                      backgroundColor:'red'}
+  }
+);
 
 Header.propTypes = {headerText: PropTypes.string.isRequired};
